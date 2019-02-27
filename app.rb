1a3580aaa8f3b8bfeaa4ac44157d0d8d6f3c2761
Mina Mekhael <mina.mekhael@ymail.com>
2019-02-27 23:07:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do
+  get '/puppy' do
     erb :index
   end
 
@@ -10,7 +10,7 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
-  post '/puppy' do
+  post '/' do
     @new_pooch = Puppy.new(params[:name], params[:breed], params[:age])
 
     erb :display_puppy
